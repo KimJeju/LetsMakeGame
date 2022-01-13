c9dccec1b50657746cb20f1f54dff86905467121
Kimjeju <kingswingsex@gmail.com>
2022-01-13 10:42:18 +0900

    For문 예제 추가

--- 22.01.07/Project3/Project3.cpp
+++ 22.01.07/Project3/Project3.cpp
@@ -160,6 +160,23 @@ int main()
 	};
 
 	/*
+	2단	3단	4단
+	5단	6단	7단
+	8단	9단	10단
+	*/
+	for (int i = 2; i < 11; i += 3)
+	{
+		for (int j = 1; j <= 9; ++j)
+		{
+			cout << i << "*" << j << ": " << i * j << "\t";
+			cout << i + 1 << "*" << j << ": " << (i + 1) * j << "\t";
+			cout << i + 2 << "*" << j << ":" << (i + 2) * j << endl;
+		};
+
+		cout << endl;
+	};
+
+	/*
 	별을 아래 형태로 출력한다
 	*
 	* *
@@ -172,22 +189,72 @@ int main()
 			cout << "*";
 
 		}
-		cout << "\n";
+		cout << endl;
 	};
 
 
 	// 위와 반대로 별을 줄인다
 
-	for (int i = 1; i <= 10; i++)
+	for (int i = 1; i <= 10; i++) //라인 수 만큼 반복
 	{
 		for (int j = 10; j >= i; j--)
 		{
 			cout << "*";
 
 		}
-		cout << "\n";
+		cout << endl;
 	};
 
+	//삼각형 모양 출력
+	for (int i = 0; i < 4; ++i)
+	{
+	 //공백을 출력하기 위한 for문
+		for (int j = 0; j < 3 - i; ++j)
+		{
+			cout << " ";
+		}
+
+		for (int j = 0; j < i * 2 + 1; ++j)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	// 공백 3,2,1,0 
+	// 별 : 1,3,5,7
+
+	// 역순별 만들기
+	int iLine = 15; 
+	int iCount = 0;
+
+	for (int i = 0; i < iLine; ++i)
+	{
+		iCount = i;
+
+		// i값이 7줄 기준 4,5,6 즉 밑의 삼각형을 구성할떄만 if문이 들어가게 된다
+		if (i >= iLine / 2)
+		{
+			iCount = iLine - 1 - i;
+		}
+
+		// i값이 0,1,2,3 일때는 iCount값이 그대로 대입된다
+		// i값이 4,5,6 일 떄는 iCount는 2,1,0이 된다
+		// 즉, 최종 i값은 0으로 돌아가게 된다
+		for (int j = 0; j < iLine / 2 - iCount; ++j)
+		{
+			cout << " ";
+		}
+
+		for (int j = 0; j < iCount * 2 + 1; ++j)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+	
+	// 공백 " 3,2,1,0,1,2,3
+	// 별 : 1,3,5,7,5,3,1
 
 	return 0;
 }
