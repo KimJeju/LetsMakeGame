a862dc25bc63761dc42b5e6ce75eb201c27fd792
Kimjeju <kingswingsex@gmail.com>
2022-02-11 15:51:24 +0900

    퇴근으로 인한 중단

--- 22.02.11/22.02.11/22.02.11.cpp
+++ 22.02.11/22.02.11/22.02.11.cpp
@@ -66,6 +66,34 @@ typedef struct _tagPoint
 {
     int x;
     int y;
+
+    _tagPoint() :
+        x(0),
+        y(0)
+    {
+    }
+
+    _tagPoint(int _x, int _y):
+        x(_x),
+        y(_y)
+    {
+    }
+
+    _tagPoint(const _tagPoint& pt)
+    {
+        //앝은 복사를 한다. 이렇게 해줄 경우 this는 자기자신의 포인터이고,
+        //*를 붙여서 자기자신을 역참조하여 모든 데이터를 복사하게 한다.
+        *this = pt;
+    }
+
+    _tagPoint operator +(const _tagPoint& pt)
+    {
+        _tagPoint result;
+        result.x = x + pt.x;
+        result.y = y + pt.y;
+        return result;
+    }
+
 }PINOT, *PPOINT;
 
 ;
@@ -147,6 +175,16 @@ int main()
 
     func4(3.14);
 
+
+    cout << " ========== Point =========" << endl;
+    PINOT pt1(10, 20), pt2(30, 40), pt3;
+
+    // POINT 구조체는 + 연산자가 operator로 재정의 되어있다
+    // 그래서 + 연산이 가능하게 되고 pt1 의 + 연산자 함수를 호출해주는 개념이다.
+    // pt2를 인자로 넘겨준다
+    pt3 = pt1 + pt2;
+
+    cout << "x :" << pt3.x << "y :" << pt3.y << endl;
 
     return 0;
 }
