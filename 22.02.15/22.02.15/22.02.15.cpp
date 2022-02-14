353aefc667f1e677a9ecfd516e4dc861630bb97b
Kimjeju <kingswingsex@gmail.com>
2022-02-14 14:04:20 +0900

    오류로 인한 중단

--- 22.02.15/22.02.15/22.02.15.cpp
+++ 22.02.15/22.02.15/22.02.15.cpp
@@ -1,21 +1,33 @@
 ﻿#include <iostream>
 using namespace std;
 
-/*
-템플릿 : 컴파일 단계에서 타입을 결정 짓는 기능이다.
-타입이 다양하게 쓰일 수 있다.
 
-template <typeName 원하는 이름> 의 형태로 사용가능하다.
-template <class 원하는 이름> 의 형태로 사용 가능하다.
-
-예) template <typename T>
-void OutPut()
+template <class T>
+void OutPuttype()
 {
-   cout << typeid(T).name() << endl;
+	cout << "=========== OutPutData ==========" << end;
+	cout << typeid(T).name() << endl;
 }
-*/
+
+template <class T>
+void OutPutData(T data)
+{
+	cout << "=========== OutPutData ==========" << end;
+	cout << typeid(T).name() << endl;
+	cout << data << endl;
+}
 
 int main()
 {
+
+	OutPuttype<int>();
+	OutPuttype<float>();
+
+	OutPutData(10);
+	OutPutData(3.1);
+	//OutPutData(382, 382.f);
+	OutPutData('a');
+	OutPutData("ㅋㅋㅋㅋㅋㅋ");
+
 	return 0;
 }
