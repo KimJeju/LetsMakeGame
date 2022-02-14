13ac917fa647014779621d7f2b0aeec159d5731d
Kimjeju <kingswingsex@gmail.com>
2022-02-14 13:37:31 +0900

    템플릿

---
+++ 22.02.15/22.02.15/22.02.15.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+템플릿 : 컴파일 단계에서 타입을 결정 짓는 기능이다.
+타입이 다양하게 쓰일 수 있다.
+
+template <typeName 원하는 이름> 의 형태로 사용가능하다.
+template <class 원하는 이름> 의 형태로 사용 가능하다.
+
+예) template <typename T>
+void OutPut()
+{
+   cout << typeid(T).name() << endl;
+}
+*/
+
+int main()
+{
+	return 0;
+}
